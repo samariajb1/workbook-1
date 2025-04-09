b3ce6b29dc8c4a98d35ea22f567fbed798339ed9
Samaria Barron <samariajb1@gmail.com>
2025-04-09 11:54:32 -0400

    completed ShoppingList app

---
+++ first-test-project/src/test/java/com/plurasight/Shoppinglist.java
@@ -0,0 +1,11 @@
+package com.plurasight;
+
+import static java.lang.System.*;
+
+public class Shoppinglist {
+    public static void main(String[] args) {
+        System.out.println("Bread,Cheese,Milk,Tea,Kale,Cabbage,Chicken,Lettuce,Orange Juice.");
+
+    }
+
+}
